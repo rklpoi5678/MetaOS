858d064f24a71733b7d07f9facbda5691783021c
rklpoi5678 <rklpoi5678@naver.com>
2025-04-22 22:45:52 +0900

    change src folder

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { supabase } from '../lib/supabaseClient';
 import { useRouter } from 'next/router';
-import { useUserStore } from '@/pages/src/store/userStore';
+import { useUserStore } from '@/src/store/userStore';
 
 export default function Login() {
   const setUser = useUserStore(state => state.setUser);
