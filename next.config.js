3fd35f754382cda8c33ee347f65d0d826cb814b6
rklpoi5678 <rklpoi5678@naver.com>
2025-04-25 03:51:32 +0900

    Fix: css

--- next.config.js
+++ next.config.js
@@ -1,13 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
   images: {
     domains: ['localhost'],
-  },
-  experimental: {
-    serverActions: true,
-  },
+  }
 }
 
 module.exports = nextConfig 
