286dc6c01c82828dfd62fe0339305c5a5223e93d
rklpoi5678 <rklpoi5678@naver.com>
2025-04-25 06:09:48 +0900

    next.config.js webpack add

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  webpack: (config) => {
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      path: false,
+    };
+    config.module.rules.push({
+      test: /\.mjs$/,
+      include: /node_modules/,
+      type: "javascript/auto"
+    });
+    return config;
+  },
+  transpilePackages: ['framer-motion'],
   images: {
     domains: ['localhost'],
   },
